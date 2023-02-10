4d8dad380abf719e268fbe9f15d3919ccea9f112
ArsVZero <ars.nsu1994@gmail.com>
2023-02-10 23:00:58 +0600

    Add project files.

---
+++ WebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationContext context;
+        public EmployeeController(ApplicationContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = context.Employees.ToList();
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                var emp = new Employee()
+                {
+                    Name = model.Name,
+                    City = model.City,
+                    State = model.State,
+                    Salary = model.Salary,
+                };
+
+                context.Employees.Add(emp);
+                context.SaveChanges();
+                TempData["error"] = "Record created!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["message"] = "Empty fields must be filled!";
+                return View(model);
+            }
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var emp = context.Employees.SingleOrDefault(e => e.Id == id);
+            var result = new Employee()
+            {
+                Name = emp.Name,
+                City = emp.City,
+                State = emp.State,
+                Salary = emp.Salary,
+            };
+            return View(result);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee model)
+        {
+            var emp = new Employee()
+            {
+                Id = model.Id,
+                Name = model.Name,
+                City = model.City,
+                State = model.State,
+                Salary = model.Salary,
+            };
+            context.Employees.Update(emp);
+            context.SaveChanges();
+            TempData["error"] = "Record edited!";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var emp = context.Employees.SingleOrDefault(e => e.Id == id);
+            context.Employees.Remove(emp);
+            context.SaveChanges();
+            TempData["error"] = "Record deleted!";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApp/Data/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
